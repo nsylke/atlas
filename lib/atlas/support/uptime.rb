8c01194e9232f9bee2db8615f46ffbc0a16cd213
Nicholas Sylke <nicksylke@outlook.com>
2018-06-12 22:12:23 -0500

    Move uptime into its own class

---
+++ lib/atlas/support/uptime.rb
@@ -0,0 +1,30 @@
+module Atlas
+	class Uptime
+		attr_reader :boot
+
+		def initialize
+			@boot = Time.now
+		end
+
+		def uptime
+			dhms.join(', ')
+		end
+
+		private
+
+		def self.dhms
+			seconds = (Time.now - @boot).to_i
+
+			days = seconds / 86400
+			seconds -= days * 86400
+			hours = seconds / 3600
+			seconds -= hours * 3600
+			minutes = seconds / 60
+			seconds -= minutes * 60
+
+			["#{days}d", "#{hours}h", "#{minutes}m", "#{seconds}s"]
+		end
+
+		private_class_method :dhms
+	end
+end
