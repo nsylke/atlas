30296c386b2310df7d4aa42baeacafee3e1ac44c
Nicholas Sylke <nicksylke@outlook.com>
2018-06-13 08:00:19 -0500

    Fixed no arguments in `choose` command

--- lib/atlas/commands/fun/choose.rb
+++ lib/atlas/commands/fun/choose.rb
@@ -3,6 +3,11 @@ module Atlas::Commands
 		extend Discordrb::Commands::CommandContainer
 
 		command(:choose) do |event, *args|
+			if args[0].nil?
+            	event.respond('No arguments specified.')
+            	break
+            end
+
 			event.respond args.join(' ').split(', ').sample
 		end
 	end
