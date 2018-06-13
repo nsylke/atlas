ce439c613314533199043a339eb28a40d15cf18b
Nicholas Sylke <nicksylke@outlook.com>
2018-06-13 10:00:09 -0500

    Fix pluralization in `ready` event for connected servers message

--- lib/atlas/events/ready.rb
+++ lib/atlas/events/ready.rb
@@ -9,7 +9,7 @@ module Atlas::Events
         	event.bot.game = "#{Atlas::CONFIG.prefix}help | #{'Server'.pluralize(event.bot.servers.length)}"
 
         	logger.info "Logged in as #{event.bot.profile.distinct} (#{event.bot.profile.id})."
-		    logger.info "Connected to #{event.bot.servers.length} servers."
+		    logger.info "Connected to #{"server".pluralize(event.bot.servers.length)}."
 		    logger.info "OAuth URL: #{event.bot.invite_url}"
         end
     end
