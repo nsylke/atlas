459d0e43d3b2f7cfdf2d271a211166ccb84ba541
Nicholas Sylke <nicksylke@outlook.com>
2018-06-13 08:00:41 -0500

    Disable bots from using `choose` command

--- lib/atlas/commands/fun/choose.rb
+++ lib/atlas/commands/fun/choose.rb
@@ -3,6 +3,8 @@ module Atlas::Commands
 		extend Discordrb::Commands::CommandContainer
 
 		command(:choose) do |event, *args|
+			return if event.author.bot_account?
+
 			if args[0].nil?
             	event.respond('No arguments specified.')
             	break
