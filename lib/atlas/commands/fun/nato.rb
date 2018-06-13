f5d5fde802fb0f10957ff6baad13f56ed73eb232
Nicholas Sylke <nicksylke@outlook.com>
2018-06-13 07:49:11 -0500

    Fix no arguments in `nato` command

--- lib/atlas/commands/fun/nato.rb
+++ lib/atlas/commands/fun/nato.rb
@@ -3,6 +3,11 @@ module Atlas::Commands
         extend Discordrb::Commands::CommandContainer
 
         command(:nato) do |event, *args|
+        	if args[0].nil?
+            	event.respond('No arguments specified.')
+            	break
+            end
+
             args = args.join(' ')
 
             event.respond(args.to_s.to_nato)
