24294617a44cb1476d344bc85cccf2eb0dddd9cc
Nicholas Sylke <nicksylke@outlook.com>
2018-06-12 22:14:09 -0500

    Fix logger in ready event

--- lib/atlas/events/ready.rb
+++ lib/atlas/events/ready.rb
@@ -2,13 +2,15 @@ module Atlas::Events
     module Ready
         extend Discordrb::EventContainer
 
+        include Atlas::Loggable
+
         ready do |event|
         	event.bot.online
         	event.bot.game = "#{Atlas::CONFIG.prefix}help | #{'Server'.pluralize(event.bot.servers.length)}"
 
-        	Atlas::LOGGER.info("[READY] Logged in as #{event.bot.profile.distinct} (#{event.bot.profile.id}).")
-		    Atlas::LOGGER.info("[READY] Connected to #{event.bot.servers.length} servers.")
-		    Atlas::LOGGER.info("[READY] OAuth URL: #{event.bot.invite_url}")
+        	logger.info "Logged in as #{event.bot.profile.distinct} (#{event.bot.profile.id})."
+		    logger.info "Connected to #{event.bot.servers.length} servers."
+		    logger.info "OAuth URL: #{event.bot.invite_url}"
         end
     end
 end
