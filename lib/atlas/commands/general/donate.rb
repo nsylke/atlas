6ac1d9ad09c88898c81428ffff378d453c59dd91
Nicholas Sylke <nicksylke@outlook.com>
2018-06-13 09:07:36 -0500

    Add `paypal`, `patreon`, and `patron` as alias to `donate` command

--- lib/atlas/commands/general/donate.rb
+++ lib/atlas/commands/general/donate.rb
@@ -2,7 +2,7 @@ module Atlas::Commands
     module Donate
         extend Discordrb::Commands::CommandContainer
 
-        command(:donate) do |event|
+        command([:donate, :paypal, :patreon, :patron]) do |event|
             return if event.author.bot_account?
 
             event.respond('Whoa, you found the secret command. We currently are not accepting donations, but thank you for the consideration of donating to Atlas!')
