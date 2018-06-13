275fa56c4c0ac0934c0ebdddbf98a60f2c17fffa
Nicholas Sylke <nicksylke@outlook.com>
2018-06-13 09:52:48 -0500

    Debug how many commands have been registered

--- lib/atlas/commands.rb
+++ lib/atlas/commands.rb
@@ -1,5 +1,7 @@
 module Atlas
     module Commands
+        include Atlas::Loggable
+
         # Load all command files
         Dir["#{File.dirname(__FILE__)}/commands/fun/*.rb"].each { |c| load c }
         Dir["#{File.dirname(__FILE__)}/commands/general/*.rb"].each { |c| load c }
@@ -64,24 +66,32 @@ module Atlas
             @fun.each do |c|
                 Atlas::BOT.include!(c)
             end
+
+            logger.debug "Registered #{@fun.count} fun commands."
         end
 
         def self.registerGeneralCommands!
             @general.each do |c|
                 Atlas::BOT.include!(c)
             end
+
+            logger.debug "Registered #{@general.count} general commands."
         end
 
         def self.registerIntegrationCommands!
             @integration.each do |c|
                 Atlas::BOT.include!(c)
             end
+
+            logger.debug "Registered #{@integration.count} integration commands."
         end
 
         def self.registerModerationCommands!
             @moderation.each do |c|
                 Atlas::BOT.include!(c)
             end
+
+            logger.debug "Registered #{@moderation.count} moderation commands."
         end
     end
 end
