d9672318c99330ace0588708b75b8a25348dd73e
Nicholas Sylke <nicksylke@outlook.com>
2018-06-13 09:15:45 -0500

    Debug messages on server_create and server_delete event

--- lib/atlas/events/server_delete.rb
+++ lib/atlas/events/server_delete.rb
@@ -5,6 +5,8 @@ module Atlas::Events
         server_delete do |event|
         	event.bot.game = "#{Atlas::CONFIG.prefix}help | #{'Server'.pluralize(event.bot.servers.length)}"
             Atlas::DATABASE.query("DELETE FROM servers WHERE id = '#{event.server.id}'")
+            logger.debug "Deleted record in database"
+        	logger.info "Atlas has left #{event.server.name}"
         end
     end
 end
