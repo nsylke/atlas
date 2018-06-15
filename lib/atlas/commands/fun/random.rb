45952290de8c8d24f932e590e425b03e64646c2a
Nicholas Sylke <nicksylke@outlook.com>
2018-06-15 08:59:27 -0500

    `random` command no longer fails if number is too large

--- lib/atlas/commands/fun/random.rb
+++ lib/atlas/commands/fun/random.rb
@@ -6,8 +6,13 @@ module Atlas::Commands
             return if event.author.bot_account?
 
             if max 
+                max = max.to_i > 2147483647 ? max = 2147483647 : max
+                
                 event.respond(rand(min.to_i..max.to_i))
             elsif min
+                min = min.to_i > 2147483647 ? min = 2147483647 : mi
+                n
+
                 event.respond(rand(0..min.to_i))
             else
                 event.respond(rand(0..2147483647))
