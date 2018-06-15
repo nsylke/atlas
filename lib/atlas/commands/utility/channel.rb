dbf45fe17e897e696c44f19c14a1095e68d86e13
Nicholas Sylke <nicksylke@outlook.com>
2018-06-15 10:59:34 -0500

    Fix channel topic

--- lib/atlas/commands/utility/channel.rb
+++ lib/atlas/commands/utility/channel.rb
@@ -7,13 +7,15 @@ module Atlas::Commands
 
             return "Please specify a channel." if args.nil?
 
-            channel = event.bot.find_channel(args, event.server).first
+            # channel = event.bot.find_channel(args, event.server).first
+            # Type 4 is a category and causes issues with topic being blank
+            channel = event.server.channels.find { |c| c.name == args && c.type != 4 }
 
             return "Channel couldn't be found." if channel.nil?
 
             event.channel.send_embed do |embed|
                 embed.title = channel.name
-                embed.description = channel.topic
+                embed.description = channel.topic || 'No topic available.'
                 embed.add_field name: 'ID', value: channel.id, inline: true
                 embed.add_field name: 'Type', value: (channel.type == 0 ? 'Text' : 'Voice'), inline: true
                 embed.add_field name: 'Position', value: channel.position, inline: true
