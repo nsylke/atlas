e70bbe227c18dbdc9ce4226e4d21bda8631ed683
Nicholas Sylke <nicksylke@outlook.com>
2018-06-17 09:35:03 -0500

    Add `online_members` and `verification_level` to `server` command

--- lib/atlas/commands/utility/server.rb
+++ lib/atlas/commands/utility/server.rb
@@ -13,8 +13,10 @@ module Atlas::Commands
                 embed.add_field name: 'ID', value: server.id, inline: true
                 embed.add_field name: 'Owner', value: "#{server.owner.distinct}", inline: true
                 embed.add_field name: 'Region', value: server.region, inline: true
-                embed.add_field name: 'Channels', value: server.channels.length, inline: true
+                embed.add_field name: 'Online Members', value: server.online_members.count, inline: true
                 embed.add_field name: 'Members', value: server.member_count, inline: true
+                embed.add_field name: 'Channels', value: server.channels.length, inline: true
+                embed.add_field name: 'Verification', value: server.verification_level.to_s.capitalize, inline: true
                 embed.add_field name: 'Roles', value: server.roles.length, inline: true
                 embed.add_field name: 'Emojis', value: server.emojis.length, inline: true
             end
