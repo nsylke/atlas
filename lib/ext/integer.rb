17b6ae418b3767813d295590aec414905f964005
Nicholas Sylke <nicksylke@outlook.com>
2018-06-17 09:33:58 -0500

    Added `Integer#to_roman` for transforming integers into roman numerals

---
+++ lib/ext/integer.rb
@@ -0,0 +1,26 @@
+class Integer
+	def to_roman
+		number = self
+
+		roman = {
+			'M' => 1000,
+			'CM' => 900,
+			'D' => 500,
+			'CD' => 400,
+			'C' => 100,
+			'XC' => 90,
+			'L' => 50,
+			'XL' => 40,
+			'X' => 10,
+			'IX' => 9,
+			'V' => 5,
+			'IV' => 4,
+			'I' => 1
+		}
+
+		roman.map do |letter, value|
+			amount, number = number.divmod(value)
+			letter * amount
+		end.join
+	end
+end
