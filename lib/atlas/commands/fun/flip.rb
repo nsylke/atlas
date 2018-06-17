86e3e22a9c42ed093ee8afb3fa6323e78e115264
Nicholas Sylke <nicksylke@outlook.com>
2018-06-17 09:30:54 -0500

    Alias `coin` for `flip` command

--- lib/atlas/commands/fun/flip.rb
+++ lib/atlas/commands/fun/flip.rb
@@ -2,7 +2,7 @@ module Atlas::Commands
     module Flip
         extend Discordrb::Commands::CommandContainer
 
-        command(:flip) do |event|
+        command([:flip, :coin]) do |event|
             return if event.author.bot_account?
 
             event.respond(%w(Heads Tails).sample)
